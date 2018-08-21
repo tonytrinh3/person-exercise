513591dc77b1a062c8af6b896ba8a6b752e4769b
Tony Trinh <tony.w.trinh3@gmail.com>
2018-08-21 09:02:33 -0700

    8-21-2018 changed person and persons from stateless to stateful components
    8-21-2018 changed person and persons from stateless to stateful components

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,20 +1,27 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import Person from './Person/Person';
 
+
 // no more this.namechangedhandler bc this is a function not a class so you need to pass something to the function in the app.js 
-const persons = (props) => {
-    return (
-        props.persons.map((person, index) => {
-                        return <Person 
-                        click = {() =>props.clicked(index)}
-                        name = {person.name}
-                        age = {person.age}
-                        key = {person.id} 
-                        changed = {(event) => props.changed(event,person.id)}/>
-                        
-                    } )
-    );
+// now extend to class to components 
+//now turn a const stateless component into a stateful component 
+class Persons extends Component{
+    render (){
+        return this.props.persons.map((person, index) => {
+            return <Person 
+            click = {() =>this.props.clicked(index)}
+            name = {person.name}
+            age = {person.age}
+            key = {person.id} 
+            changed = {(event) => this.props.changed(event,person.id)}/>
+            
+        } )
+
+
+    }
+
+
 }
 
-export default persons;
+export default Persons;
